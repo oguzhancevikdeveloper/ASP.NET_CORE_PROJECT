f519824c316ac5c01af11bd2f5b0962c1b9fb22a
oguzhan.cevik <oguzhancevik.developer@gmail.com>
2022-09-29 22:50:30 +0300

    Add project files.

---
+++ ASP.NET_CORE_PROJECT/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP.NET_CORE_PROJECT.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
