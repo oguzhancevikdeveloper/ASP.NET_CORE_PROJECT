51922d980e008fd3950b0ff1a9294c81d6810673
oguzhan.cevik <oguzhancevik.developer@gmail.com>
2022-10-20 11:56:31 +0300

    Authentication Ve Controller Seviyesinde Authorize, Kampı Vitrinde Revizeler, Contact Sayfası,  About İşlemleri.

---
+++ ASP.NET_CORE_PROJECT/Controllers/ContactController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_CORE_PROJECT.Controllers
+{
+  public class ContactController : Controller
+  {
+    ContactManager cm = new ContactManager(new EfContactRepository());
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public IActionResult Index(Contact contact)
+    {
+      contact.ContactDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+      contact.ContactStatus = true;
+      cm.AddContact(contact);
+      return View("Index","Blog");
+    }
+  }
+}
---
+++ ASP.NET_CORE_PROJECT/Controllers/WriterController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_CORE_PROJECT.Controllers
+{
+
+  //[Authorize]
+  public class WriterController : Controller
+  {
+    [Authorize]
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
