4588ef3c029f40ae538588c2304df9891f5362b4
oguzhan.cevik <oguzhancevik.developer@gmail.com>
2022-12-16 17:49:36 +0300

    Swagger Ve Api Get List, Api ile Veri Ekleme, Apiler İle Veri Getirme, Apilerle Silme Ve Güncelleme

---
+++ BlogApiDemo/DataAccessLayer/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+  public class Context : DbContext
+  {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      //optionsBuilder.UseNpgsql("Server=localhost;User Id=postgres;Port=5432;Password=123456;Database=CoreBlogDb;Pooling=true; Integrated Security =true;");
+      optionsBuilder.UseSqlServer("Server=DESKTOP-O3DVRF8\\SQLEXPRESS;User Id=DESKTOP-O3DVRF8\\oguzh;Password=;Database=CoreBlogApiDb;Pooling=true; Integrated Security =true;");
+    }
+    public DbSet<Employee> Employees { get; set; }
+  }
+}
+ 
